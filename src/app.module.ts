3dddeb11579cc270e64988b303b993035f1e5830
Mazen Othman <112982340+MazenOth@users.noreply.github.com>
2025-03-07 17:25:55 +0200

    feat(app): Add app module with sequelize connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { Author, Post } from './models';
+import { PostsModule } from './modules/posts/posts.module';
+import { AuthorsModule } from './modules/authors/authors.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      models: [Author, Post],
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    PostsModule,
+    AuthorsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
